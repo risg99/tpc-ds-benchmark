b07b3de4bf0735f9b1e28011417a6777abfb9e77
Ahmad AS <a.schigdar@gmail.com>
2022-10-26 14:36:15 +0200

    Version 2.1, Updated optimization

--- all_queries/optimized_queries_final/query-81.sql
+++ all_queries/optimized_queries_final/query-81.sql
@@ -1,31 +1,63 @@
-
-with customer_total_return as
- (select cr_returning_customer_sk as ctr_customer_sk
-        ,ca_state as ctr_state, 
- 	sum(cr_return_amt_inc_tax) as ctr_total_return
- from catalog_returns
-     ,date_dim
-     ,customer_address
- where cr_returned_date_sk = d_date_sk 
-   and d_year =1998
-   and cr_returning_addr_sk = ca_address_sk 
- group by cr_returning_customer_sk
-         ,ca_state )
-  select  c_customer_id,c_salutation,c_first_name,c_last_name,ca_street_number,ca_street_name
-                   ,ca_street_type,ca_suite_number,ca_city,ca_county,ca_state,ca_zip,ca_country,ca_gmt_offset
-                  ,ca_location_type,ctr_total_return
- from customer_total_return ctr1
-     ,customer_address
-     ,customer
- where ctr1.ctr_total_return > (select avg(ctr_total_return)*1.2
- 			  from customer_total_return ctr2 
-                  	  where ctr1.ctr_state = ctr2.ctr_state)
-       and ca_address_sk = c_current_addr_sk
-       and ca_state = 'TX'
-       and ctr1.ctr_customer_sk = c_customer_sk
- order by c_customer_id,c_salutation,c_first_name,c_last_name,ca_street_number,ca_street_name
-                   ,ca_street_type,ca_suite_number,ca_city,ca_county,ca_state,ca_zip,ca_country,ca_gmt_offset
-                  ,ca_location_type,ctr_total_return
- limit 100;
-
-
+with customer_total_return as (
+	select 
+		cr_returning_customer_sk as ctr_customer_sk ,
+		ca_state as ctr_state,
+		sum(cr_return_amt_inc_tax) as ctr_total_return
+	from 
+		catalog_returns
+		inner join date_dim on cr_returned_date_sk = d_date_sk
+		inner join customer_address on cr_returning_addr_sk = ca_address_sk
+		where 
+			d_year = 1998
+		group by 
+			cr_returning_customer_sk
+			,ca_state)			
+			
+, cust_average_return as (
+	select 
+		ctr_state as ctr_state
+	,avg(ctr_total_return) * 1.2 as ctr_avg_return
+	from customer_total_return ctr1
+	group by ctr_state
+	)
+	
+select c_customer_id,
+	c_salutation,
+	c_first_name,
+	c_last_name,
+	ca_street_number,
+	ca_street_name ,
+	ca_street_type,
+	ca_suite_number,
+	ca_city,
+	ca_county,
+	ca_state,
+	ca_zip,
+	ca_country,
+	ca_gmt_offset ,
+	ca_location_type,
+	ctr_total_return
+from customer_total_return ctr1
+	inner join cust_average_return ctr2 on ctr1.ctr_state = ctr2.ctr_state
+	inner join customer on ctr_customer_sk = c_customer_sk
+	inner join customer_address on c_current_addr_sk = ca_address_sk
+where 
+	ctr1.ctr_total_return > ctr2.ctr_avg_return
+	and ca_state = 'TX'
+order by c_customer_id,
+	c_salutation,
+	c_first_name,
+	c_last_name,
+	ca_street_number,
+	ca_street_name ,
+	ca_street_type,
+	ca_suite_number,
+	ca_city,
+	ca_county,
+	ca_state,
+	ca_zip,
+	ca_country,
+	ca_gmt_offset ,
+	ca_location_type,
+	ctr_total_return
+limit 100;
